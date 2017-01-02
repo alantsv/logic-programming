59a59c078ad17a34704902a19f4cfef407d36468
Alan Veloso <alantsv@gmail.com>
2017-01-02 16:06:42 -0300

    CHECKPOINT 02-Jan-17

--- aula-02-Jan-17.pl
+++ aula-02-Jan-17.pl
@@ -1,10 +1,37 @@
 %24- Reverse com acumulador
 
-	reverse(L,R):-reverse(L,[],R).
-	reverse ([],R,R).
-	reverse([X|Xs], ACC,R) :- reverse(Xs,[X|ACC],R)
+	reverse2(L,R):-reverse2(L,[],R).
+	reverse2([],R,R).
+	reverse2([X|Xs], ACC,R) :- reverse2(Xs,[X|ACC],R).
 
 %25- Reverso com recursividade
 	
 	reverso(L,R) :- L=[], R=[].
-	reverso(L,R) :- L=[X|Xs], reverso(Xs, Xr), append(Xs, [X], R).
+	reverso(L,R) :- L=[X|Xs], reverso(Xs, Xr), append(Xr, [X], R).
+
+%27- Lista palindrome?
+	
+	palindrome(L) :- reverse(L,R), L=R.
+
+%27.1- Lista palindrome? (Outra versão)
+
+	palindrome1(L):- L=[X|Xs], append(Xm, [U], Xs), X=U, palindrome1(Xm).
+	palindrome1([]):-!.
+	palindrome1([X]):-!.
+%28 - Metade iguais?
+	
+	metIguais(L):- append(M,M,L).
+
+% Primeiros N elementos da lista L
+
+	primeiros(L,N,L1):- length(L1,N), append(L1,_,L).
+
+	%primeiros([a,b,c,d],2,L1). L1=[a,b]
+
+% Usando primeiros e reverse faça metades iguais
+
+metIguais2(XY):- length(XY,L), M is L/2,
+		primeiros(XY,M,X),
+		reverse(XY,YX),
+		primeiros(YX,M,Yr),
+		reverse(Yr, Y), Y = X.
