4c3b1499cd4a17b2250d416d6f1ed582a35ff3fd
Alan Veloso <alantsv@gmail.com>
2017-01-04 16:22:30 -0300

    CHECKPOINT 04-Jan-17

--- aula-04-Jan-17.pl
+++ aula-04-Jan-17.pl
@@ -43,4 +43,29 @@
 
 %?- insDir([1,7,1,4,5,7]),L.
 
+%39- Particiona
 
+	particiona([X,Y|XYs],[X|Xs],[Y|Ys]):-particiona(XYs,Xs,Ys).
+	particiona([X],[X],[]).
+	particiona([],[],[]).
+
+%?- partiociona([a,b,c,1,2,3,4],A,B). A=[a,c,2,4], B=[b,1,3]
+
+%40- 
+
+	merge1([X|Xs],[Y|Ys],[X|XYs]):- X@<Y,!,merge1(Xs,[Y|Ys],XYs).
+	merge1([X|Xs],[Y|Ys],[Y|XYs]):- X@>Y,!,merge1([X|Xs],Ys,XYs).
+	merge1([],Ys,Ys):-!.
+	merge1(Xs,[],Xs):-!.
+
+%?- merge1([a,b,b,k,z],[c,m,n,o],X). X=[a,b,b,c,k,m,n,o,z]
+
+%41- ERRO!
+
+	mergeSort([],[]):-!.
+	mergeSort([X],[X]):-!.
+	mergeSort(L,S):- particiona(L,X,Y),
+			mergeSort(X,Xo),mergeSort(Y,Yo),
+			merge1(Xo,Yo,S).
+
+%?- mergeSort([1,7,1,7,3,66,2,1,0], S).
