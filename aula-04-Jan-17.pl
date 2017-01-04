58bd6b39c6233546397cbf992c6cb6260cf123ac
Alan Veloso <alantsv@gmail.com>
2017-01-04 15:51:41 -0300

    CHECKPOINT 04-Jan-17

--- aula-04-Jan-17.pl
+++ aula-04-Jan-17.pl
@@ -13,8 +13,34 @@
 	permutacao([],[]).
 
 %?- select(5,[1,3,5,7], X). X=[1,3,7]
-%?- permutacao([1,7,5,3],X). X = [1,3,5,7];... X=[1,7,3,5] ... ... X=[1,3,5,7]...
+%?- permutacao([1,7,5,3],X). X = [1,3,5,7] ; ... X=[1,7,3,5] ; ... ... X=[1,3,5,7]
 
 	sotx(L,S):- permutacao(L,S), isOrdered(S).
 
 %?- sotx([1,7,5,3], X). X= [1,3,5,7]
+
+%36- Inserção odernada
+
+	insOrd(X,[],[X]).
+	insOrd(X,[Y|Ys], [X, Y|Ys]):- X<Y.
+	insOrd(X, [Y|Ys], [Y|XYs]):- X>=Y, insOrd(X,Ys,XYs).
+
+%?- insOrd(4,[2,3,5,7],L). L=[2,3,4,5,7]
+
+%37- Inserção ordenada (Pode usar letras) 
+
+	insOrd1(X,[],[X]).
+	insOrd1(X,[Y|Ys], [X, Y|Ys]):- X@<Y.
+	insOrd1(X, [Y|Ys], [Y|XYs]):- X@>=Y, insOrd1(X,Ys,XYs).
+
+%?- insOrd1(d,[a,b,c,f,g], L). L= [a,b,c,d,f,g]
+
+%38- Inserção direta com acumulador
+
+	insDir(L,S):- insDir(L,[],S).
+	insDir([X|Xs], Acc, S):- insOrd(X, Acc, Acc1), insDir(Xs,Acc1,S).
+	insDir([],Acc,S):- S=Acc,!.
+
+%?- insDir([1,7,1,4,5,7]),L.
+
+
