310d220f8e89c23b485afcf22026c734159bda97
Alan <alantsv@gmail.com>
2017-02-08 16:03:34 -0300

    CHECKPOINT 08-Feb-17

---
+++ aula/aula-08-Feb-17.pl
@@ -0,0 +1,8 @@
+% a) Quais são todos os nós de partida do grafo?
+% b) Quais são todos os nós de chega do grafo?
+% c) Quais são os nós do grafo?
+% d) Quais são todos os caminhos entre A e F?
+% e) Qual é o menor caminho entre A e F?
+% f) Qual é o maior caminho entre A e F?
+
+%:-[map].
